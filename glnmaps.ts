6f25692c168cda8318f726ced3b273f588323230
Junya Ishihara <junya@champierre.com>
2022-10-08 00:30:44 +0900

    Applied "deno fmt".

--- glnmaps.ts
+++ glnmaps.ts
@@ -2,7 +2,7 @@ import { readCSVObjects } from "https://deno.land/x/csv/mod.ts";
 import { serve } from "https://deno.land/std@0.159.0/http/server.ts";
 
 serve(handler, { port: 3000 });
-console.log('glnmaps is running. Access it at: http://localhost:3000/');
+console.log("glnmaps is running. Access it at: http://localhost:3000/");
 const csvPath = Deno.args[0];
 const geojson = await csv2geojson(csvPath);
 const index = await Deno.readTextFile("./index.html");
@@ -15,8 +15,8 @@ async function csv2geojson(_csvPath: string) {
 
   let features = [];
   for await (const row of readCSVObjects(f)) {
-    const lat = Number(row['緯度'] || row['緯度（10進法）']);
-    const lng = Number(row['経度'] || row['経度（10進法）']);
+    const lat = Number(row["緯度"] || row["緯度（10進法）"]);
+    const lng = Number(row["経度"] || row["経度（10進法）"]);
     const data = {
       type: "Feature",
       geometry: {
@@ -24,9 +24,9 @@ async function csv2geojson(_csvPath: string) {
         coordinates: [lng, lat],
       },
       properties: {
-        title: row['名称'],
-        description: row['名称'],
-      }
+        title: row["名称"],
+        description: row["名称"],
+      },
     };
     features.push(data);
   }
@@ -34,8 +34,8 @@ async function csv2geojson(_csvPath: string) {
 
   const json = {
     type: "FeatureCollection",
-    features: features
-  }
+    features: features,
+  };
   return JSON.stringify(json);
 }
 
