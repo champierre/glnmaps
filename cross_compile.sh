fad4c24c62cad19ecaffed056b5741fc8f94b67b
Junya Ishihara <junya@champierre.com>
2022-10-07 19:16:41 +0900

    Install deno.

--- cross_compile.sh
+++ cross_compile.sh
@@ -1,4 +1,4 @@
 deno compile --allow-read --allow-net --target aarch64-apple-darwin --output binaries/aarch64-apple-darwin-glnmaps glnmaps.ts
 deno compile --allow-read --allow-net --target x86_64-apple-darwin --output binaries/x86_64-apple-darwin-glnmaps glnmaps.ts
-deno compile --allow-read --allow-net --target x86_64-pc-windows-msvc --output  x86_64-pc-windows-msvc-glnmaps glnmaps.ts
-deno compile --allow-read --allow-net --target x86_64-unknown-linux-gnu --output  x86_64-unknown-linux-gnu-glnmaps glnmaps.ts
+deno compile --allow-read --allow-net --target x86_64-pc-windows-msvc --output binaries/x86_64-pc-windows-msvc-glnmaps glnmaps.ts
+deno compile --allow-read --allow-net --target x86_64-unknown-linux-gnu --output binaries/x86_64-unknown-linux-gnu-glnmaps glnmaps.ts
