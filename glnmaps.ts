e902b01bbea791ed6bf36eadec727ebf214d13fb
Junya Ishihara <junya@champierre.com>
2022-10-26 00:00:22 +0900

    Enable to update data.

--- glnmaps.ts
+++ glnmaps.ts
@@ -1,4 +1,4 @@
-import { readCSVObjects } from "https://deno.land/x/csv/mod.ts";
+import { readCSVObjects, writeCSV, writeCSVObjects } from "https://deno.land/x/csv/mod.ts";
 import { serve } from "https://deno.land/std@0.159.0/http/server.ts";
 import * as Colors from "https://deno.land/std@0.146.0/fmt/colors.ts";
 
@@ -9,6 +9,12 @@ if (!csvPath) {
   );
   Deno.exit(1);
 }
+
+const f = await Deno.open(csvPath);
+const spots: any = []
+for await (const row of readCSVObjects(f)) {
+  spots.push(row);
+};
 
 serve(handler, { port: 3000 });
 console.log(Colors.green("glnmaps is running. Access it at: http://localhost:3000/"));
@@ -56,19 +62,48 @@ let index = `<!DOCTYPE html>
 
 index = index.replace("{{geojson}}", geojson);
 
-function description(_row: any) {
-  let desc = "";
+function description(id: Number, _row: any) {
+  let desc = `<a href="/spots/${id}/edit">編集</a><br />`;
   for (const key in _row) {
     desc += `<strong>${key}:</strong> ${_row[key]}<br />`;
   }
   return desc;
 }
 
-async function csv2geojson(_csvPath: string) {
-  const f = await Deno.open(_csvPath);
+function editPage(spotId, spotData) {
+  let edit = `<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>edit</title>
+    <style>
+      input[type="text"] {
+        width: 300px;
+      }
+    </style>
+  </head>
+  <body>
+    <form action="/spots/${spotId}" method="post">
+      <table>`;
+  let index = 0;
+  for (const key in spotData) {
+    edit += `<tr><td><label for="${key}">${key}</label></td><td><input type="text" name="${key}" value="${spotData[key]}" /></td></tr>`;
+    index++;
+  }
+  edit += `
+        </table>
+      <input type="submit" value="更新" />
+    </form>
+  </body>
+</html>`;
+  return edit;
+}
 
+async function csv2geojson(_csvPath: string) {
   const features = [];
-  for await (const row of readCSVObjects(f)) {
+  let id = 0;
+  for await (const row of spots) {
     const lat = Number(row["緯度"] || row["緯度（10進法）"] || row["lat"] || row["latitude"]);
     const lng = Number(row["経度"] || row["経度（10進法）"] || row["lng"] || row["longitude"] || row["lon"] || row["long"]);
     const data = {
@@ -79,10 +114,11 @@ async function csv2geojson(_csvPath: string) {
       },
       properties: {
         title: row["名称"] || row["name"],
-        description: description(row),
+        description: description(id, row),
       },
     };
     features.push(data);
+    id++;
   }
   f.close();
 
@@ -93,16 +129,49 @@ async function csv2geojson(_csvPath: string) {
   return JSON.stringify(json);
 }
 
-function handler(req: Request): Response {
+async function handler(req: Request): Response {
   const url = new URL(req.url);
+  const method = req.method;
   let html;
 
-  if (url.pathname == "/") {
+  const editPattern = new URLPattern({ pathname: "/spots/:id/edit" });
+  const updatePattern = new URLPattern({ pathname: "/spots/:id" });
+
+  if (url.pathname === "/") {
     html = index.replace("{{download-link}}", downloadLink);
     return new Response(html, {
       headers: { "content-type": "text/html" },
     });
-  } else if (url.pathname == "/download") {
+  } else if (updatePattern.test({ pathname: url.pathname }) && method === "POST") {
+    const match = updatePattern.exec({ pathname: url.pathname });
+    const spotId = match.pathname.groups.id;
+
+    const bodyReader = await req.body?.getReader().read();
+    const decoder = new TextDecoder();
+    const body = decoder.decode(bodyReader?.value);
+
+    const params = new URLSearchParams(decodeURI(body));
+    const spot = Object.fromEntries(params.entries());
+    spots[spotId] = spot;
+
+    const header = Object.keys(spots[0]);
+    const csvData = spots.map((row) => 
+      Object.values(row).map((value) => value.toString())
+    );
+    const file = await Deno.open("new." + csvPath, { write: true, create: true, truncate: true });
+    await writeCSV(file,[header, ...csvData]);
+
+    return new Response(`post ${spotId}`, {
+      headers: { "content-type": "text/html" },
+    });
+  } else if (editPattern.test({ pathname: url.pathname })) {
+    const match = editPattern.exec({ pathname: url.pathname });
+    const spotId = match.pathname.groups.id;
+    const spotData = spots[Number(spotId)];
+    return new Response(editPage(spotId, spotData), {
+      headers: { "content-type": "text/html" },
+    });
+  } else if (url.pathname === "/download") {
     html = index.replace("{{download-link}}", "");
     return new Response(html, {
       headers: { "content-type": "text/plain" },
